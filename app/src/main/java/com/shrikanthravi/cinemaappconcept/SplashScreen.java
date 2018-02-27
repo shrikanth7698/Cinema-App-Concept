7a3159a46bfb2f8f7a8e2beab4aad060c5515665
shrikanth7698 <shrikanth7698@gmail.com>
2018-02-27 21:22:57 +0530

    First Commit

---
+++ app/src/main/java/com/shrikanthravi/cinemaappconcept/SplashScreen.java
@@ -0,0 +1,13 @@
+package com.shrikanthravi.cinemaappconcept;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
